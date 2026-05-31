9471eb182d34f92aed3674c6dc1614f8593ac00d
Rathallah03 <rathallah03@gmail.com>
2026-05-31 22:54:06 +0700

    Final VM

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import mysql.connector
@@ -108,6 +109,157 @@ def update_product(product_id):
     return jsonify({
         "message": "Produk berhasil diperbarui"
     })
+    
+@app.route('/api/transactions', methods=['POST'])
+def add_transaction():
+
+    data = request.json
+
+    product_id = data['product_id']
+    qty = int(data['qty'])
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT * FROM products WHERE id = %s",
+        (product_id,)
+    )
+
+    product = cursor.fetchone()
+
+    if not product:
+
+        conn.close()
+
+        return jsonify({
+            "message": "Produk tidak ditemukan"
+        }), 404
+
+    if qty > product['stok']:
+
+        conn.close()
+
+        return jsonify({
+            "message": "Stok tidak mencukupi"
+        }), 400
+
+    subtotal = float(product['harga']) * qty
+
+    cursor.execute(
+        """
+        INSERT INTO transactions
+        (tanggal, total)
+        VALUES (%s, %s)
+        """,
+        (
+            datetime.now(),
+            subtotal
+        )
+    )
+
+    transaction_id = cursor.lastrowid
+
+    cursor.execute(
+        """
+        INSERT INTO transaction_items
+        (
+            transaction_id,
+            product_id,
+            qty,
+            subtotal
+        )
+        VALUES (%s, %s, %s, %s)
+        """,
+        (
+            transaction_id,
+            product_id,
+            qty,
+            subtotal
+        )
+    )
+
+    cursor.execute(
+        """
+        UPDATE products
+        SET stok = stok - %s
+        WHERE id = %s
+        """,
+        (
+            qty,
+            product_id
+        )
+    )
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({
+        "message": "Transaksi berhasil"
+    })
+    
+@app.route('/api/dashboard', methods=['GET'])
+def dashboard():
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT COUNT(*) AS total_produk FROM products"
+    )
+
+    total_produk = cursor.fetchone()['total_produk']
+
+    cursor.execute(
+        "SELECT COUNT(*) AS total_transaksi FROM transactions"
+    )
+
+    total_transaksi = cursor.fetchone()['total_transaksi']
+
+    cursor.execute(
+        """
+        SELECT
+            IFNULL(SUM(total),0)
+            AS total_penjualan
+        FROM transactions
+        """
+    )
+
+    total_penjualan = cursor.fetchone()['total_penjualan']
+
+    conn.close()
+
+    return jsonify({
+        "total_produk": total_produk,
+        "total_transaksi": total_transaksi,
+        "total_penjualan": total_penjualan
+    })
+    
+@app.route('/api/transactions', methods=['GET'])
+def get_transactions():
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT
+            t.id,
+            p.nama_produk,
+            ti.qty,
+            ti.subtotal
+        FROM transactions t
+        JOIN transaction_items ti
+            ON t.id = ti.transaction_id
+        JOIN products p
+            ON p.id = ti.product_id
+        ORDER BY t.id DESC
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return jsonify(data)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
